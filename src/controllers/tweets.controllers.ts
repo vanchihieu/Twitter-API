3b8f6a5684f9eee35cc1320997b1d0055b820e6f
Van Chi Hieu <vanchihieu3@gmail.com>
2025-06-01 21:08:42 +0700

    feat: Implement getTweetChildren functionality with pagination and view tracking

--- src/controllers/tweets.controllers.ts
+++ src/controllers/tweets.controllers.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express'
 import { ParamsDictionary } from 'express-serve-static-core'
-import { TweetRequestBody } from '~/models/requests/Tweet.requests'
+import { TweetType } from '~/constants/enum'
+import { TweetParam, TweetQuery, TweetRequestBody } from '~/models/requests/Tweet.requests'
 import { TokenPayload } from '~/models/requests/User.requests'
 import tweetsService from '~/services/tweets.services'
 
@@ -25,5 +26,31 @@ export const getTweetController = async (req: Request, res: Response) => {
     return res.json({
         message: 'Get Tweet Successfully',
         result: tweet
+    })
+}
+
+export const getTweetChildrenController = async (req: Request<TweetParam, any, any, TweetQuery>, res: Response) => {
+    const tweet_type = Number(req.query.tweet_type) as TweetType
+    const limit = Number(req.query.limit)
+    const page = Number(req.query.page)
+    const user_id = req.decoded_authorization?.user_id
+
+    const { total, tweets } = await tweetsService.getTweetChildren({
+        tweet_id: req.params.tweet_id,
+        tweet_type,
+        limit,
+        page,
+        user_id
+    })
+
+    return res.json({
+        message: 'Get Tweet Children Successfully',
+        result: {
+            tweets,
+            tweet_type,
+            limit,
+            page,
+            total_page: Math.ceil(total / limit)
+        }
     })
 }
